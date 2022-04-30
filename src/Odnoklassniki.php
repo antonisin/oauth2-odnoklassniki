8ded32e9a577ef634e9f0e3004af9e73ec687bb6
antonisin <maxim.antonisin@gmail.com>
2022-04-30 08:56:17 +0300

    Updated type return declaration

--- src/Odnoklassniki.php
+++ src/Odnoklassniki.php
@@ -7,6 +7,7 @@ use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
 use League\OAuth2\Client\Token\AccessToken;
 use Psr\Http\Message\ResponseInterface;
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
 
 class Odnoklassniki extends AbstractProvider
 {
@@ -20,7 +21,7 @@ class Odnoklassniki extends AbstractProvider
     /**
      * {@inheritdoc}
      */
-    public function getBaseAuthorizationUrl()
+    public function getBaseAuthorizationUrl(): string
     {
         return 'https://www.odnoklassniki.ru/oauth/authorize';
     }
@@ -28,7 +29,7 @@ class Odnoklassniki extends AbstractProvider
     /**
      * {@inheritdoc}
      */
-    public function getBaseAccessTokenUrl(array $params)
+    public function getBaseAccessTokenUrl(array $params): string
     {
         return 'https://api.odnoklassniki.ru/oauth/token.do';
     }
@@ -36,19 +37,20 @@ class Odnoklassniki extends AbstractProvider
     /**
      * {@inheritdoc}
      */
-    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    public function getResourceOwnerDetailsUrl(AccessToken $token): string
     {
         $param = 'application_key='.$this->clientPublic
             .'&fields=uid,name,first_name,last_name,location,pic_3,gender,locale,photo_id'
             .'&method=users.getCurrentUser';
-        $sign = md5(str_replace('&', '', $param).md5($token.$this->clientSecret));
+	$sign = md5(str_replace('&', '', $param).md5($token.$this->clientSecret));
+
         return 'http://api.odnoklassniki.ru/fb.do?'.$param.'&access_token='.$token.'&sig='.$sign;
     }
 
     /**
      * {@inheritdoc}
      */
-    protected function getDefaultScopes()
+    protected function getDefaultScopes(): array
     {
         return [];
     }
@@ -56,7 +58,7 @@ class Odnoklassniki extends AbstractProvider
     /**
      * {@inheritdoc}
      */
-    protected function checkResponse(ResponseInterface $response, $data)
+    protected function checkResponse(ResponseInterface $response, $data): void
     {
         if (isset($data['error_code'])) {
             throw new IdentityProviderException($data['error_msg'], $data['error_code'], $response);
@@ -69,7 +71,7 @@ class Odnoklassniki extends AbstractProvider
     /**
      * {@inheritdoc}
      */
-    protected function createResourceOwner(array $response, AccessToken $token)
+    protected function createResourceOwner(array $response, AccessToken $token): ResourceOwnerInterface
     {
         return new OdnoklassnikiResourceOwner($response);
     }
